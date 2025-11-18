feb74da3b2b6328a35d497738c8477d788dc4476
SarangaKalhari <kalharisaranga9@gmail.com>
2025-11-18 12:43:28 +0530

    Create hibernate file and make and connect database

---
+++ src/main/java/org/example/controller/MemberController.java
@@ -0,0 +1,20 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+
+    @GetMapping
+    public String hello(){
+        return "Hello";
+    }
+
+
+    public void addMember(){
+    }
+
+
+}
