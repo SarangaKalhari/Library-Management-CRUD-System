4c03c5c3174b0ec087d5b39d365527fa57475527
SarangaKalhari <kalharisaranga9@gmail.com>
2026-01-19 21:23:32 +0530

    Add HibernateUtil for Hibernate SessionFactory setup

---
+++ src/main/java/org/example/config/HibernateUtil.java
@@ -0,0 +1,23 @@
+package org.example.config;
+
+import org.example.model.entity.Member;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    private static Configuration configuration = new Configuration();
+
+    public static SessionFactory getSessionFactory(){
+        if (null == sessionFactory){
+            configuration.configure();
+            configuration.addAnnotatedClass(Member.class);
+            sessionFactory = configuration.buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+
+
+}
