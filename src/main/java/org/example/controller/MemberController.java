a264ae51e1beafa0044e17e79086dbfc87345b87
SarangaKalhari <kalharisaranga9@gmail.com>
2026-01-19 20:51:13 +0530

    Implement Book and Member controllers

--- src/main/java/org/example/controller/MemberController.java
+++ src/main/java/org/example/controller/MemberController.java
@@ -1,19 +1,37 @@
 package org.example.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.model.entity.Member;
+import org.example.service.MemberService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MemberController {
+
+    MemberService memberService = new MemberService();
 
     @GetMapping
     public String hello(){
         return "Hello";
     }
 
+    @PostMapping("/add")
+    public void addMember(@RequestBody Member member){
+        memberService.addMember(member);
+    }
 
-    public void addMember(){
+    @PostMapping("/update")
+    public void updateMember(@RequestBody Member member){
+        memberService.updateMember(member);
+    }
+
+    @DeleteMapping("/delete")
+    public void deleteMember(@RequestBody String id){
+        memberService.deleteMember(id);
+    }
+
+    @GetMapping("/search/{id}")
+    public Member searchMember(@PathVariable String id){
+        return memberService.searchMember(id);
     }
 
 
